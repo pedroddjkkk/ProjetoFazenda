cf6e15dcce916525728a155b9384a0ce1e68b26c
Pedro Valério <pedroddjkK@gmail.com>
2022-12-19 20:52:48 -0300

    Estilização verde

--- client/src/index.js
+++ client/src/index.js
@@ -1,23 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Navbar from './Navbar/Navbar';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Navbar from "./Navbar/Navbar";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-let arr = [];
+const root = ReactDOM.createRoot(document.getElementById("root"));
+let pages = [];
 
-arr.push({name: 'Home', path: '/home', element: <div>home</div>, icon: "fa-solid fa-house"});
-arr.push({name: 'Relatorios', path: '/relatorios', element: <div>about</div>, icon: "fa-solid fa-flag"});
-arr.push({name: 'Bois', path: '/bois', element: <div>contact</div>, icon: "fa-solid fa-cow"});
+pages.push({
+  name: "Home",
+  path: "/home",
+  element: <div className="main">home</div>,
+  icon: "fa-solid fa-house",
+});
+pages.push({
+  name: "Relatorios",
+  path: "/relatorios",
+  element: <div className="main">about</div>,
+  icon: "fa-solid fa-flag",
+});
+pages.push({
+  name: "Bois",
+  path: "/bois",
+  element: <div className="main">contact</div>,
+  icon: "fa-solid fa-cow",
+});
+pages.push({
+  name: "Cavalos",
+  path: "/cavalos",
+  element: <div className="main">contact</div>,
+  icon: "fa-solid fa-horse",
+});
 
-const router = createBrowserRouter(arr);
+const router = createBrowserRouter(pages);
 
 root.render(
   <React.StrictMode>
-    <Navbar content={arr} />
+    <Navbar content={pages} />
     <RouterProvider router={router} />
   </React.StrictMode>
 );
