a6a4133066b952bedc960b7e5e800ebdd000b3f3
Pedro Valério <pedroddjkK@gmail.com>
2022-12-20 14:53:19 -0300

    Implementação navbar Bootstrap

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import "../../App.css";
+import "../../assets/bootstrap/css/bootstrap.min.css"
+
+function App({content}) {
+  return (
+    <nav class="navbar navbar-dark align-items-start sidebar sidebar-dark accordion bg-gradient-primary p-0">
+    <div class="container-fluid d-flex flex-column p-0"><a class="navbar-brand d-flex justify-content-center align-items-center sidebar-brand m-0" href="#">
+            <div class="sidebar-brand-icon rotate-n-15"><i class="fas fa-tractor"></i></div>
+            <div class="sidebar-brand-text mx-3"><span>Agro</span></div>
+        </a>
+        <hr class="sidebar-divider my-0" />
+        <ul id="accordionSidebar" class="navbar-nav text-light">
+          {content.map((item) => (
+            <li className="nav-item"><a className="nav-link" href={item.path} ><i className={"fas " + item.icon}></i><span>{item.name}</span></a></li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Navbar from "./Navbar/Navbar";
+import Navbar from "./components/Navbar/Navbar";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
