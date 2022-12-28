089580c90a4f961be5f9be16dde7b997e375d8a4
Pedro Valério <pedroddjkK@gmail.com>
2022-12-28 20:27:47 -0300

    Bois com sequelize totalmente funcional

--- server/model/Boi.js
+++ server/model/Boi.js
@@ -1,46 +1,67 @@
-const dbUtils = require("../utils/dbUtils");
+const sequelize = require("../utils/dbUtils").sequelize;
+
+const { DataTypes, Model, Op } = require("sequelize");
+
+class Boi extends Model {
+  otherPublicField;
+}
+
+Boi.init(
+  {
+    id_pk: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    raca: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    peso: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    tableName: "tab_bois",
+    timestamps: true,
+    updatedAt: "dr_hr_atualizacao",
+    createdAt: "dr_hr_criacao",
+  }
+);
 
 async function buscar(pk, filtro) {
   if (filtro) {
-    var sql;
-    if (isNaN(filtro)) {
-      sql = `
-      SELECT 
-        * 
-      FROM 
-        tab_bois AS a 
-      WHERE 
-        a.raca like '${filtro}'`;
+    if(isNaN(filtro)) {
+      const ret = await Boi.findAll({
+        where: {
+          raca: {
+            [Op.like]: `%${filtro}%`,
+          },
+        },
+      });
+      return ret;
     } else {
-      sql = `
-      SELECT 
-        * 
-      FROM 
-        tab_bois AS a 
-      WHERE 
-        a.id_pk = ${filtro}`;
+      const ret = await Boi.findAll({
+        where: {
+          id_pk: filtro,
+        },
+      });
+      return ret;
     }
-    const ret = await dbUtils.query(sql);
-    return ret;
   } else {
-    sql = `
-    SELECT 
-      * 
-    FROM 
-      tab_bois`;
-    const ret = await dbUtils.query(sql);
+    const ret = await Boi.findAll();
     return ret;
   }
 }
 
 async function salvar(where, registro) {
-  const sql = `
-  INSERT INTO
-    tab_bois
-  SET
-    peso = ${registro.peso},
-    raca = '${registro.raca}'`;
-  const ret = await dbUtils.query(sql);
+  const ret = await Boi.create({
+    raca: registro.raca,
+    peso: registro.peso,
+  });
+
   return ret;
 }
 module.exports = {
