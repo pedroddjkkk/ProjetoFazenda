f796e9aa507e9cef4675789908e728de7b312c4f
Pedro Valério <pedroddjkK@gmail.com>
2023-01-08 21:37:58 -0300

    FadeIn Tela Permissões

--- src/components/Permissoes/index.jsx
+++ src/components/Permissoes/index.jsx
@@ -1,5 +1,14 @@
+import { useEffect, useRef } from "react"
+import fadeIn from "../../utils/fadeIn";
+
 export default function Permissoes(){
+  const fadeInRef = useRef(null);
+
+  useEffect(() => {
+    fadeIn(fadeInRef.current);
+  }, []);
+
   return (
-    <></>
+    <div ref={fadeInRef}></div>
   )
 }
