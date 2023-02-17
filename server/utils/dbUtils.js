8b19a168564288e002ee790d3a2a6e7c37dae45b
Pedro Aparecido Valerio <pedroddjkk@gmail.com>
2023-02-17 09:11:02 -0300

    Adding .env.example

--- server/utils/dbUtils.js
+++ server/utils/dbUtils.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("fazenda", "root", "", {
+const sequelize = new Sequelize(process.env.DB, "root", "root", {
   host: "localhost",
   dialect: "mysql",
 });
