6e770214afe26f8c0ecef09e629325a41f736a37
Pedro Aparecido Valerio <pedroddjkk@gmail.com>
2023-02-17 09:22:49 -0300

    .env Applied to DB

--- server/utils/dbUtils.js
+++ server/utils/dbUtils.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DB, "root", "root", {
-  host: "localhost",
-  dialect: "mysql",
+const sequelize = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT || "mysql",
 });
 
 async function tryConnection() {
