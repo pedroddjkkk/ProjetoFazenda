2a3858f53d4838ba980cc091ae838c2e64492c70
Pedro Aparecido Valério <pedroddjkk@gmail.com>
2023-02-25 11:21:19 -0300

    Entity Lote

---
+++ server/model/Lote.js
@@ -0,0 +1,80 @@
+const sequelize = require("../utils/dbUtils").sequelize;
+
+const { DataTypes, Model } = require("sequelize");
+
+class Lote extends Model {
+  otherPublicField;
+}
+
+Lote.init(
+  {
+    id_pk: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    nome: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+  },
+  {
+    sequelize,
+    tableName: "tab_lotes",
+    timestamps: true,
+    updatedAt: "dr_hr_atualizacao",
+    createdAt: "dr_hr_criacao",
+  }
+);
+
+async function buscar(pk, filtro) {
+  if (pk){
+    const ret = await Lote.findAll({
+      where: {
+        id_pk: pk,
+      },
+    });
+
+    return ret;
+  } else {
+    const ret = await Lote.findAll();
+    return ret;
+  }
+}
+
+async function salvar(where, registro) {
+  if (where) {
+    const ret = await Lote.update(
+      {
+        nome: registro.nome,
+      },
+      {
+        where: {
+          id_pk: where,
+        },
+      }
+    );
+
+    return ret;
+  } else {
+    const ret = await Lote.create({
+      nome: registro.nome,
+    });
+
+    return ret;
+  }
+}
+
+async function excluir(pk, filtro){
+  await Lote.destroy({
+    where: {
+      id_pk: pk,
+    },
+  });
+}
+
+module.exports = {
+  buscar,
+  salvar,
+  excluir,
+};
