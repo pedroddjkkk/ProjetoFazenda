0ff06d505c47f382e0f85ca6708e037dbb6ca56c
Pedro Valério <pedroddjkK@gmail.com>
2023-03-11 20:50:32 -0300

    Validator Implemented (Node Code)

--- src/index.js
+++ src/index.js
@@ -10,80 +10,83 @@ import Usuarios from "./components/Usuarios/Usuarios";
 import Fazendas from "./components/Fazendas/Fazendas";
 import Racoes from "./components/Rações/Racoes";
 import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 import Dashboard from "./components/DashBoard";
-import "./App.css"
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import "./App.css";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import Permissoes from "./components/Permissoes";
 import Acompanhamento from "./components/Acompanhamento/Acompanhamento";
+import Validator from "./services/Validator";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 let pages = [];
 
 pages.push({
-    navigable: true,
-    element: <Navbar pages={pages}/>,
-    children: [
-        {
-          navigable: true,
-          name: "Dashboard",
-          path: "/dashboard",
-          element: <Dashboard />,
-          icon: "fa-solid fa-gauge",
-        },
-        {
-            navigable: true,
-            name: "Usuários",
-            path: "/users",
-            element: <Usuarios />,
-            icon: "fa-solid fa-user",
-        },
-        {
-            navigable: true,
-            name: "Fazendas",
-            path: "/fazendas",
-            element: <Fazendas />,
-            icon: "fa-solid fa-house-user",
-        },
-        {
-            navigable: true,
-            name: "Rações",
-            path: "/racoes",
-            element: <Racoes />,
-            icon: "fa-solid fa-utensils",
-        },
-        {
-          navigable: true,
-          name: "Acompanhar Bovinos",
-          path: "/acompanhamento",
-          element: <Acompanhamento />,
-          icon: "fa-solid fa-chart-line",
-        },
-        {
-          navigable: true,
-          name: "Permissões",
-          path: "/permissoes",
-          element: <Permissoes />,
-          icon: "fa-solid fa-lock",
-        }
-    ],
+  navigable: true,
+  element: <Navbar pages={pages} />,
+  children: [
+    {
+      navigable: true,
+      name: "Dashboard",
+      path: "/dashboard",
+      element: <Dashboard />,
+      icon: "fa-solid fa-gauge",
+    },
+    {
+      navigable: true,
+      name: "Usuários",
+      path: "/users",
+      element: <Usuarios />,
+      icon: "fa-solid fa-user",
+    },
+    {
+      navigable: true,
+      name: "Fazendas",
+      path: "/fazendas",
+      element: <Fazendas />,
+      icon: "fa-solid fa-house-user",
+    },
+    {
+      navigable: true,
+      name: "Rações",
+      path: "/racoes",
+      element: <Racoes />,
+      icon: "fa-solid fa-utensils",
+    },
+    {
+      navigable: true,
+      name: "Acompanhar Bovinos",
+      path: "/acompanhamento",
+      element: <Acompanhamento />,
+      icon: "fa-solid fa-chart-line",
+    },
+    {
+      navigable: true,
+      name: "Permissões",
+      path: "/permissoes",
+      element: <Permissoes />,
+      icon: "fa-solid fa-lock",
+    },
+  ],
 });
 pages.push({
-    navigable: false,
-    name: "Login",
-    path: "/",
-    element: <Login />,
+  navigable: false,
+  name: "Login",
+  path: "/",
+  element: <Login />,
 });
 
 const router = createBrowserRouter(pages);
 
 root.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <RouterProvider router={router} />
-        </Provider>
-        <ToastContainer />
-    </React.StrictMode>
+  <React.StrictMode>
+    <Validator>
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
+      <ToastContainer />
+    </Validator>
+  </React.StrictMode>
 );
 
 serviceWorkerRegistration.register();
