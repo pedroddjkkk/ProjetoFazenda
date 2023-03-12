949c79dde44b388bb268dd399b1240c912364ebd
Pedro Valério <pedroddjkK@gmail.com>
2023-03-12 19:31:58 -0300

    Production Connection

--- server/utils/dbUtils.js
+++ server/utils/dbUtils.js
@@ -1,11 +1,17 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DB || "fazenda", process.env.DB_USER || "root", process.env.DB_PASSWORD || "", {
-  host: process.env.DB_HOST || "localhost",
-  dialect: process.env.DB_DIALECT || "mysql",
-  port: process.env.DB_PORT || 3306,
-});
+let sequelize;
+if(true) {
+  sequelize = new Sequelize("postgres://root:Xinelo24@@localhost:5432/fazenda");
+} else {
+  sequelize = new Sequelize(process.env.DB || "fazenda", process.env.DB_USER || "root", process.env.DB_PASSWORD || "", {
+    host: process.env.DB_HOST || "localhost",
+    dialect: process.env.DB_DIALECT || "mysql",
+    port: process.env.DB_PORT || 3306,
+  });
+}
+
 
 async function tryConnection() {
   try {
