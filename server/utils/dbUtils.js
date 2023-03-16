cfdeae4a349c37e4f47c89535533f6925e0accc8
Pedro Aparecido Valério <pedroddjkk@gmail.com>
2023-03-16 08:11:10 -0300

    ENV for Production

--- server/utils/dbUtils.js
+++ server/utils/dbUtils.js
@@ -1,6 +1,10 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
+if (process.env.NODE_ENV === "production") {
+  console.log("Conectando ao banco de dados em produção");
+}
+
 const sequelize = new Sequelize(process.env.DB || "fazenda", process.env.DB_USER || "root", process.env.DB_PASSWORD || "", {
   host: process.env.DB_HOST || "localhost",
   dialect: process.env.DB_DIALECT || "mysql",
